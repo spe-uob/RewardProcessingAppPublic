2b3f547a0ece01bd6f9e53a9825e1c4538f67ce5
Kaush-06 <tu21960@bristol.ac.uk>
2023-02-10 20:41:52 +0000

    Changed locations under directories and added code
    Added a branch for the game part , changed directories to game, and added initial code.
    More code to follow once I fix my flutter command line tools' issues.

---
+++ RewardProcessing/lib/game/gameMain.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'gamehomepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget { // widget cannot change when the user interacts with it, hence stateless 
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // to remove the defalut banner
+      home: HomePage(),
+      
+    );
+  }
+}
