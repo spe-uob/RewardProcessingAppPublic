8581bb4a963cccad66bf783ebe8170953d7374d0
ww_plr <98458713+wwplr@users.noreply.github.com>
2023-02-10 18:11:22 +0000

    Update main.dart

--- RewardProcessing/lib/main.dart
+++ RewardProcessing/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rewardprocessing/questionnaire/prolificID.dart';
+import 'package:rewardprocessing/questionnaire/prolific_id.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
