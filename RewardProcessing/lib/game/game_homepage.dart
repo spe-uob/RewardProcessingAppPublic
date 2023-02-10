122933a11bcbb3e7e599650e93a93fbceccb5397
Kaush-06 <99398227+Kaush-06@users.noreply.github.com>
2023-02-10 21:11:47 +0000

    Update game_homepage.dart

--- RewardProcessing/lib/game/game_homepage.dart
+++ RewardProcessing/lib/game/game_homepage.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:rewardprocessing/game/game_main.dart';
 
+class MyApp extends StatelessWidget { // widget cannot change when the user interacts with it, hence stateless 
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // to remove the defalut banner
+      home: HomePage(),
+
+    );
+  }
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
