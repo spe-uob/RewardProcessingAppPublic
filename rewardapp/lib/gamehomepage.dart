d4cef773eb3909e9cdef7704c17dc7b9bce5e329
Kaush@06 <tu21960@bristol.ac.uk.com>
2023-02-10 20:06:14 +0000

    Changed locations under directories and added code

---
+++ rewardapp/lib/gamehomepage.dart
@@ -0,0 +1,37 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'Gamemain.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // using scaffold widget as the main parent of the page
+    int numberInRow=11;
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(children: [
+        Expanded(
+          flex: 5,
+          child: Container(
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: numberInRow),
+                itemBuilder: null),
+          ),
+        ),
+        Expanded(
+          child: Container(color: Colors.pink),
+        )
+      ]),
+    );
+  }
+
+  Null get newMethod => null;
+}
+cd
