d3118b659aef8ef7810dec4e95f8f69617812f8b
1Haixin <78486970+1Haixin@users.noreply.github.com>
2023-02-27 21:14:16 +0800

    delete unused import
    delete unused import that fix failed CI

--- RewardProcessing/lib/game/game_finished.dart
+++ RewardProcessing/lib/game/game_finished.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:rewardprocessing/questionnaire/finishedquestion.dart';
 import 'package:flutter/services.dart';
 
