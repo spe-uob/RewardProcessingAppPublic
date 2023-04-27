977c7d627d48adbd0f10683722a6ec5a54262b00
Kaush-06 <tu21960@bristol.ac.uk>
2023-04-27 14:19:41 +0100

    Game instructions fix
    Small fix for the first game Instruction's script

--- RewardProcessing/lib/game/game_instructions.dart
+++ RewardProcessing/lib/game/game_instructions.dart
@@ -46,7 +46,7 @@ class _GameInstructionsState extends State<GameInstructions> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Image.asset(
-                    'assets/images/gi.png',
+                    'assets/images/GI.png',
                     scale: 2.9,
                   ),
                 ],
